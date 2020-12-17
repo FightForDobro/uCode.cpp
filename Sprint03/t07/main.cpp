a17384186789d4d1c8a4704c07a11a2482faca26
fightfordobro <fightfordobro@gmail.com>
2020-12-17 18:05:49 +0200

    quick fix

--- Sprint03/t07/main.cpp
+++ Sprint03/t07/main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char** argv)
     std::vector<int> arr {1, 2, 3, 2, 1};
     std::string radar = "radar";
     sortValues(radar.begin(), radar.end());
-//    std::cout << res << std::endl;
+    std::cout << radar << std::endl;
     return 0;
 }
